a99d444611f86e4315280e0addfee67beebb60ae
afzalhp <rajibhossain.php@gmail.com>
2019-06-19 16:57:23 +0600

    updated

--- rh-progress-bar.php
+++ rh-progress-bar.php
@@ -45,6 +45,6 @@ function disable_plugin_deactivation($actions, $plugin_file)
 
 function isMobileDevice()
 {
-    return 1;
+//    return 1;
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
